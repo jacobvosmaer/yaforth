c2205ec3bdf03708e65a245058678f31d4438685
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-05-26 10:51:05 +0200

    Control flow madness

--- forth.c
+++ forth.c
@@ -23,17 +23,35 @@ struct entry {
 };
 
 struct {
-  int stackp, compiling;
-  int stack[1024];
-  struct entry *latest, dict[1024];
+  int stackp, rstackp, pc, compiling;
+  int stack[1024], rstack[1024];
+  struct entry *latest, *internal, dict[1024];
   char *error, *token;
 } state;
 
-int mem[8192], nmem;
+int mem[8192], nmem = 2; /* reserve space for interpret */
+
+struct entry *find(struct entry *start, char *word) {
+  struct entry *de;
+  for (de = start; de >= state.dict; de--)
+    if (!(de->flags & F_HIDDEN) && !strcmp(de->word, word))
+      return de;
+  return 0;
+}
 
 void entryreset(struct entry *e) {
   struct entry empty = {0};
   *e = empty;
+}
+
+void rstackpush(int x) {
+  assert(state.rstackp < nelem(state.rstack));
+  state.rstack[state.rstackp++] = x;
+}
+
+int rstackpop(void) {
+  assert(state.rstackp > 0);
+  return state.rstack[--state.rstackp];
 }
 
 char tokbuf[256];
@@ -74,6 +92,8 @@ char *gettoken(void) {
   assert(0);
 }
 
+void next(void) { state.pc++; }
+
 void defprint(int *def, int deflen) {
   fprintf(stderr, "def:");
   while (deflen-- > 0)
@@ -86,6 +106,7 @@ void stackprint(void) {
   printf("<%d>", state.stackp);
   for (i = 0; i < state.stackp; i++)
     printf(" %d", state.stack[i]);
+  next();
 }
 
 int stackpop(int *x) {
@@ -108,24 +129,28 @@ void print(void) {
   int x;
   if (stackpop(&x))
     printf(" %d", x);
+  next();
 }
 
 void add(void) {
   int x, y;
   if (stackpop(&y) && stackpop(&x))
     stackpush(x + y);
+  next();
 }
 
 void sub(void) {
   int x, y;
   if (stackpop(&y) && stackpop(&x))
     stackpush(x - y);
+  next();
 }
 
 void mul(void) {
   int x, y;
   if (stackpop(&y) && stackpop(&x))
     stackpush(x * y);
+  next();
 }
 
 void divi(void) {
@@ -136,6 +161,7 @@ void divi(void) {
     else
       state.error = "division by zero";
   }
+  next();
 }
 
 void dup(void) {
@@ -144,16 +170,25 @@ void dup(void) {
     stackpush(x);
     stackpush(x);
   }
+  next();
 }
 
-void clr(void) { state.stackp = 0; }
+void clr(void) {
+  state.stackp = 0;
+  next();
+}
 
 enum { DEFNUM = -1, DEFJUMPZ = -2, DEFEND = -3 };
 
 void endcompiling(void) {
+  struct entry *exit = find(state.internal, "exit");
+  assert(exit);
+  assert(nmem < nelem(mem) - 2);
+  mem[nmem++] = exit - state.dict;
   mem[nmem++] = DEFEND;
   state.latest->flags &= ~F_HIDDEN;
   state.compiling = 0;
+  next();
 }
 
 char *Strdup(char *s) {
@@ -179,23 +214,30 @@ void startcompiling(void) {
     assert(state.latest->word = Strdup(state.token));
     state.latest->def = mem + nmem;
   }
+  next();
 }
 
 void emit(void) {
   int x;
   if (stackpop(&x))
     putchar(x);
+  next();
 }
 
-void immediate(void) { state.latest->flags |= F_IMMEDIATE; }
+void immediate(void) {
+  state.latest->flags |= F_IMMEDIATE;
+  next();
+}
 
 void compileif(void) {
   mem[nmem++] = DEFJUMPZ;
   stackpush(nmem++);
+  next();
 }
 
 void compilethen(void) {
   int x;
+  next();
   /* retrieve start of if body */
   if (!stackpop(&x))
     return;
@@ -208,15 +250,20 @@ void equal(void) {
   int x, y;
   if (stackpop(&y) && stackpop(&x))
     stackpush(x == y);
+  next();
 }
 
 void greaterthan(void) {
   int x, y;
   if (stackpop(&y) && stackpop(&x))
     stackpush(x > y);
+  next();
 }
 
-void recursive(void) { state.latest->flags &= ~F_HIDDEN; }
+void recursive(void) {
+  state.latest->flags &= ~F_HIDDEN;
+  next();
+}
 
 void swap(void) {
   int x, y;
@@ -224,11 +271,13 @@ void swap(void) {
     stackpush(y);
     stackpush(x);
   }
+  next();
 }
 
 void drop(void) {
   int x;
   stackpop(&x);
+  next();
 }
 
 void over(void) {
@@ -238,6 +287,74 @@ void over(void) {
     stackpush(y);
     stackpush(x);
   }
+  next();
+}
+
+void docol(void) {
+  while (mem[state.pc] > DEFEND) {
+    if (mem[state.pc] == DEFNUM) {
+      stackpush(mem[++state.pc]);
+      state.pc++;
+    } else if (mem[state.pc] == DEFJUMPZ) {
+      int x;
+      state.pc++;
+      if (stackpop(&x) && !x) {
+        state.pc += mem[state.pc];
+      }
+      state.pc++;
+    } else {
+      struct entry *de = state.dict + mem[state.pc];
+      assert(de >= state.dict && de <= state.latest);
+      if (de->func) {
+        de->func();
+      } else {
+        rstackpush(state.pc);
+        state.pc = de->def - mem;
+      }
+    }
+  }
+}
+
+int asnum(char *token, int *out) {
+  char *end = 0;
+  *out = strtol(token, &end, 10);
+  return !*end;
+}
+
+void interpret(void) {
+  int x;
+  struct entry *de;
+  if (state.token = gettoken(), !state.token)
+    exit(0);
+  if (de = find(state.latest, state.token), de >= state.dict) {
+    int i = de - state.dict;
+    if (state.compiling && de->flags & F_NOCOMPILE) {
+      state.error = "word cannot be used while compiling";
+    } else if (!state.compiling && de->flags & F_COMPILE) {
+      state.error = "word can only be used while compiling";
+    } else if (state.compiling && !(de->flags & F_IMMEDIATE)) {
+      mem[nmem++] = i;
+    } else {
+      mem[0] = i;
+      mem[1] = DEFEND;
+      state.pc = 0;
+      docol();
+    }
+  } else if (asnum(state.token, &x)) {
+    if (state.compiling) {
+      mem[nmem++] = DEFNUM;
+      mem[nmem++] = x;
+    } else {
+      stackpush(x);
+    }
+  } else {
+    state.error = "unknown word";
+  }
+}
+
+void exit_(void) {
+  state.pc = rstackpop();
+  next();
 }
 
 void initState(void) {
@@ -260,72 +377,19 @@ void initState(void) {
                              {"swap", swap},
                              {"drop", drop},
                              {"over", over},
-                             {".s", stackprint}};
+                             {".s", stackprint},
+                             {"interpret", interpret},
+                             {"exit", exit_}};
   memset(&state, 0, sizeof(state));
   assert(sizeof(initdict) <= sizeof(state.dict));
   memmove(state.dict, initdict, sizeof(initdict));
   state.latest = state.dict + nelem(initdict) - 1;
-}
-
-void docol(int *def) {
-  for (; *def > DEFEND; def++) {
-    if (*def == DEFNUM) {
-      stackpush(*++def);
-    } else if (*def == DEFJUMPZ) {
-      int x;
-      def++;
-      if (stackpop(&x) && !x) {
-        def += *def;
-      }
-    } else {
-      struct entry *de = state.dict + *def;
-      assert(de >= state.dict && de <= state.latest);
-      if (de->func)
-        de->func();
-      else
-        docol(de->def);
-    }
-  }
-}
-
-int asnum(char *token, int *out) {
-  char *end = 0;
-  *out = strtol(token, &end, 10);
-  return !*end;
+  state.internal = state.latest;
 }
 
 int main(void) {
   initState();
-  while ((state.token = gettoken())) {
-    int x;
-    struct entry *de;
-    for (de = state.latest; de >= state.dict; de--)
-      if (!(de->flags & F_HIDDEN) && !strcmp(de->word, state.token))
-        break;
-    if (de >= state.dict) { /* token found in dict */
-      int i = de - state.dict;
-      if (state.compiling && de->flags & F_NOCOMPILE) {
-        state.error = "word cannot be used while compiling";
-      } else if (!state.compiling && de->flags & F_COMPILE) {
-        state.error = "word can only be used while compiling";
-      } else if (state.compiling && !(de->flags & F_IMMEDIATE)) {
-        mem[nmem++] = i;
-      } else {
-        int def[2];
-        def[0] = i;
-        def[1] = DEFEND;
-        docol(def);
-      }
-    } else if (asnum(state.token, &x)) {
-      if (state.compiling) {
-        mem[nmem++] = DEFNUM;
-        mem[nmem++] = x;
-      } else {
-        stackpush(x);
-      }
-    } else {
-      state.error = "unknown word";
-    }
-  }
+  while (1)
+    interpret();
   return 0;
 }
