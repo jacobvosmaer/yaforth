b0e5f2ee40e4241da9a94562b6cc3ff6b9572512
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-05-30 22:44:42 +0200

    Add ."

--- builtin.f
+++ builtin.f
@@ -4,9 +4,14 @@
 : if immediate ' branch0 , here 0 , ;
 : then immediate dup here swap - swap ! ;
 : not 0 = ;
+: <> = not ;
 : and not not swap not not * ;
 : or not swap not and not ;
 : begin immediate here ;
 : again immediate ' branch , here - , ;
 : while immediate ' branch0 , here 0 , ;
 : repeat immediate ' branch , swap here - , dup here swap - swap ! ;
+: ."
+  begin key dup 32 = while drop repeat emit
+  begin key dup dup 34 <> swap -1 > and while emit repeat drop
+;
