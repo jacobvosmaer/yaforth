9201d8447973ac14336ef3647ee4bbe7ceb9dfe6
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-05-31 18:19:49 +0200

    Add automatic trailing newline to builtin

--- builtin.c
+++ builtin.c
@@ -5,6 +5,6 @@ int main(void) {
   printf("#ifndef BUILTIN_H\n#define BUILTIN_H\nchar builtinforth[]={");
   while (c = getchar(), c != EOF)
     printf(" %d,", c);
-  printf("};\n#endif\n");
+  printf("%d};\n#endif\n", '\n');
   return 0;
 }
