c1ef383a03acac9afd354ba8abd9500ec4ffc709
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-06-05 13:21:40 +0200

    builtin.f: organize into sections

--- builtin.f
+++ builtin.f
@@ -1,21 +1,25 @@
 \ This startup file gets interpreted before forth starts
 \ reading from stdin.
 
-: rot >r swap r> swap ;
-: over >r dup r> swap ;
-: cr 10 emit ;
 : if immediate ' branch0 , here 0 , ;
+: else immediate ' branch , here 0 , swap dup here swap - swap ! ;
 : then immediate dup here swap - swap ! ;
-: not 0 = ;
-: <> = not ;
-: and not not swap not not * ;
-: or not swap not and not ;
+
 : begin immediate here ;
 : again immediate ' branch , here - , ;
 : while immediate ' branch0 , here 0 , ;
 : repeat immediate ' branch , swap here - , dup here swap - swap ! ;
+
+: rot >r swap r> swap ;
+: over >r dup r> swap ;
+
+: not 0 = ;
+: <> = not ;
+: and not not swap not not * ;
+: or not swap not and not ;
+
+: cr 10 emit ;
 : ."
   key drop \ skip exactly 1 blank after first quote
   begin key dup dup 34 <> swap -1 > and while emit repeat drop
 ;
-: else immediate ' branch , here 0 , swap dup here swap - swap ! ;
